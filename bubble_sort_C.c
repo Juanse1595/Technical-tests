e250d074c10370023642425f98f15418868650d9
Juanse1595 <3309@holberstonschool.com>
2022-04-24 16:01:50 -0500

    Finished algorithm, ready for tests, added print statements to main

--- bubble_sort_C.c
+++ bubble_sort_C.c
@@ -1,2 +1,28 @@
 /* Make a function that receives an array as a parameter and sorts it
 This example is using Bubble sort */
+
+int *bubble_sort(int *array, int n){
+  int idx, idx_2, aux;
+  for(idx = 0; idx < n; idx++){
+    for(idx_2 = 0; idx_2 < n - idx - 1; idx_2++){
+      if(array[idx_2] > array[idx_2 + 1]){
+        aux = array[idx_2];
+        array[idx_2] = array[idx_2 + 1];
+        array[idx_2 + 1] = aux;
+      }
+    }
+  }
+  return (array);
+}
+
+int main(void) {
+  int i;
+  int *array = {4, 5, -9, 2, 4, -1};
+  int n = sizeof(array) / sizeof(array[0]);
+  bubble_sort(array, n);
+  printf("{");
+  for(i = 0; i < n - 1; i++) {
+    printf("%i, ", array[i]);
+  }
+  printf("%i}\n", array[i]);
+}
