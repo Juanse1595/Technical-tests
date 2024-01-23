aa9b3aebdea83aad7216c372163893558efd949c
Juanse1595 <jspbescos15@gmail.com>
2024-01-22 20:50:09 -0500

    UPD countNumbers.py added a new solution. ADD countNumbers.ts doing the same test but in Typescript

--- countNumbers.py
+++ countNumbers.py
@@ -1,6 +1,16 @@
 # Given a list of numbers, print them and their number of occurrences
+from typing import List, Dict
 
-def countOcurrences(numbers: list):
+def countOcurrences(numbers: List[int]):
+    '''
+    This function receives a list of numbers and print a summary of how
+    many times the number is in that list
+    
+    @numbers: list of numbers to analyze
+    
+    A "summary" dictionary is created to have each number and the number of times
+    it appears
+    '''
     summary = {}
     for number in numbers:
         if number not in summary.keys():
@@ -9,5 +19,18 @@ def countOcurrences(numbers: list):
             summary[number] += 1
     for entry in summary.items():
         print(f"The number {entry[0]} appears {entry[1]} times")
+        
+def countOcurrences2(numbers: List[int]):
+    '''
+    This function receives a list of numbers and print a summary of how
+    many times the number is in that list
     
-countOcurrences([1, 1, 3, 2, 1, 1])
+    @numbers: list of numbers to analyze
+    
+    A "setNumbers" set is created to have the unique numbers of the list
+    '''
+    setNumbers = set(numbers)
+    for num in setNumbers:
+        print(f"The number {num} appears {numbers.count(num)} times")
+    
+countOcurrences2([1, 1, 3, 2, 1, 1])
---
+++ countNumbers.ts
@@ -0,0 +1,8 @@
+// Given a list of numbers, print them and their number of occurrences
+
+const countNumberInList = (numbers: Number[]): void => {
+  // Iterate over the list 
+  for (const num of numbers) {
+
+  }
+}
